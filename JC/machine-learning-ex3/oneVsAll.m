dc122dd1868b109a050ccc15b6e5a143ec3645f2
JCLee <leejchris21@gmail.com>
2016-08-21 11:57:53 -0700

    Added previous week's Logistic regression. I'll need to revise it since I messed up theta(0)

--- JC/machine-learning-ex3/oneVsAll.m
+++ JC/machine-learning-ex3/oneVsAll.m
@@ -49,6 +49,18 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% Set Initial theta
+initial_theta = zeros(n + 1, 1);
+
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+
+% Run fmincg to obtain the optimal theta
+% This function will return theta and the cost 
+[theta] = ...
+    fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+            initial_theta, options);
+
 
 
 
